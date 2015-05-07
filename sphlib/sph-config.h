d3771da1504f9912d5dc52987d311dbbf9cbac2f
ufasoft <dev@ufasoft.com>
2015-05-07 12:44:35 +0500

    -

--- sphlib/sph-config.h
+++ sphlib/sph-config.h
@@ -2,10 +2,11 @@
 
 #pragma warning(disable: 4242 4244)
 
+#define SPH_SMALL_FOOTPRINT 1
 #define SPH_NO_64 0
-#define SPH_BIG_ENDIAN 0		//!!!
+#define SPH_BIG_ENDIAN (UCFG_LITLE_ENDIAN)
 
-#if UCFG_MSC_VERSION
+#if !UCFG_GNUC_VERSION
 #	define SPH_I386_GCC 0
 #	define SPH_AMD64_GCC 0
 #	define SPH_SPARCV9_GCC 0
