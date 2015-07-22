5898ddf1c9133f3b7009c088df94d2e7c0e8cea1
ufasoft <dev@ufasoft.com>
2015-07-23 00:56:47 +0500

    ADD: protobuf library

---
+++ google/protobuf/protobuf-config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+#define _CRT_SECURE_NO_DEPRECATE 1
+#define _SCL_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_DEPRECATE
+//#define _HAS_ITERATOR_DEBUGGING	0
+
+#pragma warning(disable: 4018 4244 4715 4800)
+
