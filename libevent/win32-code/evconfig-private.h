f1e504e0fc343053bd78365bd7cf3dae6e29fe9f
ufasoft <dev@ufasoft.com>
2016-11-21 00:37:29 +0500

    update evutil

---
+++ libevent/win32-code/evconfig-private.h
@@ -0,0 +1,9 @@
+#if !defined(EVENT_EVCONFIG__PRIVATE_H_) && !defined(__MINGW32__)
+#define EVENT_EVCONFIG__PRIVATE_H_
+
+
+#pragma warning(disable: 4100 4115 4131 4152 4244 4245 4295 4456 4457 4706 4996)
+
+/* Nothing to see here.  Move along. */
+
+#endif
